ebfdf3ee1ae1f22943a24b17ac6615c218ff21ad
CazkeDK <cazke119@gmail.com>
2023-11-22 10:39:52 +0100

    Tetris

--- 3rd - tetris/sketch.js
+++ 3rd - tetris/sketch.js
@@ -1,124 +1,156 @@
-let snake;
-let food;
-let scl;
-let BackButton;
-let score = 0;
+
+const rows = 20;
+const cols = 10;
+const blockSize = 30;
+
+let board = [];
+let currentPiece;
+let gameOver = false;
 
 function setup() {
-    let canvasSize = min(windowWidth, windowHeight - document.body.clientHeight);
-    let canvas = createCanvas(canvasSize, canvasSize);
-    canvas.style('display', 'block');
-    canvas.style('margin', 'auto');
-    scl = canvasSize / 30;
-    snake = new Snake();
-    frameRate(10);
-    pickLocation();
-    BackButton = select('#BackButton')
-    BackButton.mousePressed(GetTheFuckBack);
-}
-
-function pickLocation() {
-    let cols = floor(width/scl);
-    let rows = floor(height/scl);
-    food = createVector(floor(random(cols)), floor(random(rows)));
-    food.mult(scl);
-}
-
-function GetTheFuckBack(){
-    var currentPath = window.location.pathname;
-    var newPath = currentPath.substring(0, currentPath.lastIndexOf('/'));
-    window.location.href = newPath + "/../index.html";
+    createCanvas(cols * blockSize, rows * blockSize);
+    initializeBoard();
+    spawnPiece();
 }
 
 function draw() {
-    background(51);
+    background(220);
+    if (!gameOver) {
+        updateGame();
+        drawBoard();
+        drawPiece();
+    } else {
+        showGameOver();
+    }
+}
 
-    if (snake.eat(food)) {
-        pickLocation();
-        score++;
+function initializeBoard() {
+    for (let row = 0; row < rows; row++) {
+        board[row] = [];
+        for (let col = 0; col < cols; col++) {
+            board[row][col] = 0;
+        }
+    }
+}
+
+function spawnPiece() {
+    currentPiece = new Piece();
+}
+
+function updateGame() {
+    if (currentPiece.canMoveDown()) {
+        currentPiece.moveDown();
+    } else {
+        currentPiece.lock();
+        if (currentPiece.isGameOver()) {
+            gameOver = true;
+        } else {
+            spawnPiece();
+        }
+    }
+}
+
+function drawBoard() {
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            if (board[row][col] === 1) {
+                fill(0);
+            } else {
+                fill(255);
+            }
+            rect(col * blockSize, row * blockSize, blockSize, blockSize);
+        }
+    }
+}
+
+function drawPiece() {
+    const { x, y, shape } = currentPiece;
+    for (let row = 0; row < shape.length; row++) {
+        for (let col = 0; col < shape[row].length; col++) {
+            if (shape[row][col] === 1) {
+                fill(0);
+                rect((col + x) * blockSize, (row + y) * blockSize, blockSize, blockSize);
+            }
+        }
+    }
+}
+
+function showGameOver() {
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    fill(255, 0, 0);
+    text("Game Over", width / 2, height / 2);
+}
+
+class Piece {
+    constructor() {
+        this.x = Math.floor(cols / 2) - 1;
+        this.y = 0;
+        this.shape = this.getRandomShape();
     }
 
-    snake.death();
-    snake.update();
-    snake.show();
+    getRandomShape() {
+        const shapes = [
+            [[1, 1], [1, 1]], // Square
+            [[1, 1, 1, 1]], // Line
+            [[1, 1, 1], [0, 1, 0]], // T-shape
+            [[1, 1, 0], [0, 1, 1]], // Z-shape
+            [[0, 1, 1], [1, 1, 0]], // S-shape
+            [[1, 1, 1], [0, 0, 1]], // L-shape
+            [[1, 1, 1], [1, 0, 0]] // J-shape
+        ];
+        const randomIndex = Math.floor(Math.random() * shapes.length);
+        return shapes[randomIndex];
+    }
 
-    fill(255, 0, 100);
-    rect(food.x, food.y, scl, scl);
+    canMoveDown() {
+        for (let row = 0; row < this.shape.length; row++) {
+            for (let col = 0; col < this.shape[row].length; col++) {
+                if (this.shape[row][col] === 1) {
+                    const nextRow = this.y + row + 1;
+                    if (nextRow >= rows || board[nextRow][this.x + col] === 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
 
-    textSize(scl);
-    fill(255);
-    text(`Score: ${score}`, scl, scl * 2);
+    moveDown() {
+        this.y++;
+    }
+
+    lock() {
+        for (let row = 0; row < this.shape.length; row++) {
+            for (let col = 0; col < this.shape[row].length; col++) {
+                if (this.shape[row][col] === 1) {
+                    board[this.y + row][this.x + col] = 1;
+                }
+            }
+        }
+    }
+
+    isGameOver() {
+        for (let col = 0; col < cols; col++) {
+            if (board[0][col] === 1) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 function keyPressed() {
-    if ((key === 'w' || keyCode === UP_ARROW) && snake.yspeed !== 1) {
-        snake.dir(0, -1);
-    } else if ((key === 's' || keyCode === DOWN_ARROW) && snake.yspeed !== -1) {
-        snake.dir(0, 1);
-    } else if ((key === 'd' || keyCode === RIGHT_ARROW) && snake.xspeed !== -1) {
-        snake.dir(1, 0);
-    } else if ((key === 'a' || keyCode === LEFT_ARROW) && snake.xspeed !== 1) {
-        snake.dir(-1, 0);
-    }
-}
-
-class Snake {
-    constructor() {
-        this.x = 0;
-        this.y = 0;
-        this.xspeed = 1;
-        this.yspeed = 0;
-        this.total = 0;
-        this.tail = [];
-    }
-
-    dir(x, y) {
-        this.xspeed = x;
-        this.yspeed = y;
-    }
-
-    update() {
-        if (this.total === this.tail.length) {
-            for (let i = 0; i < this.tail.length-1; i++) {
-                this.tail[i] = this.tail[i+1];
-            }
-        }
-        this.tail[this.total-1] = createVector(this.x, this.y);
-
-        this.x = this.x + this.xspeed*scl;
-        this.y = this.y + this.yspeed*scl;
-
-        this.x = constrain(this.x, 0, width-scl);
-        this.y = constrain(this.y, 0, height-scl);
-    }
-
-    show() {
-        fill(255);
-        for (let i = 0; i < this.tail.length; i++) {
-            rect(this.tail[i].x, this.tail[i].y, scl, scl);
-        }
-        rect(this.x, this.y, scl, scl);
-    }
-
-    eat(pos) {
-        let d = dist(this.x, this.y, pos.x, pos.y);
-        if (d < 1) {
-            this.total++;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    death() {
-        for (let i = 0; i < this.tail.length; i++) {
-            let pos = this.tail[i];
-            let d = dist(this.x, this.y, pos.x, pos.y);
-            if (d < 1) {
-                score = 0;
-                this.total = 0;
-                this.tail = [];
-            }
+    if (!gameOver) {
+        if (keyCode === LEFT_ARROW) {
+            currentPiece.moveLeft();
+        } else if (keyCode === RIGHT_ARROW) {
+            currentPiece.moveRight();
+        } else if (keyCode === DOWN_ARROW) {
+            currentPiece.moveDown();
+        } else if (keyCode === UP_ARROW) {
+            currentPiece.rotate();
         }
     }
 }
