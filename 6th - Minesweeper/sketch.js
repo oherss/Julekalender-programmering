b83d4fc3f6e53fdc76a675dddd62cf8b7f1fceb8
CazkeDK <cazke119@gmail.com>
2023-12-07 11:31:16 +0100

    minesweeper

--- 6th - Minesweeper/sketch.js
+++ 6th - Minesweeper/sketch.js
@@ -1,24 +1,30 @@
-
 const rows = 10;
 const cols = 10;
 const totalMines = 10;
 
-let board = [];
+let grid = [];
+let gameOver = false;
+let bombsFound = 0;
 
 function setup() {
-  createCanvas(400, 400);
-  initializeBoard();
+  
+  let canvas = createCanvas(400, 400);
+  canvas.style('display', 'block');
+  canvas.style('margin', 'auto');
+  createGrid();
   placeMines();
   calculateNumbers();
 }
 
-function initializeBoard() {
+function createGrid() {
   for (let i = 0; i < rows; i++) {
-    board[i] = [];
+    grid[i] = [];
     for (let j = 0; j < cols; j++) {
-      board[i][j] = {
-        isMine: false,
-        isRevealed: false,
+      grid[i][j] = {
+        x: i * width / rows + width / (2 * rows),
+        y: j * height / cols + height / (2 * cols),
+        revealed: false,
+        mine: false,
         number: 0
       };
     }
@@ -26,13 +32,13 @@ function initializeBoard() {
 }
 
 function placeMines() {
-  let minesPlaced = 0;
-  while (minesPlaced < totalMines) {
-    const row = floor(random(rows));
-    const col = floor(random(cols));
-    if (!board[row][col].isMine) {
-      board[row][col].isMine = true;
-      minesPlaced++;
+  let count = 0;
+  while (count < totalMines) {
+    let i = floor(random(rows));
+    let j = floor(random(cols));
+    if (!grid[i][j].mine) {
+      grid[i][j].mine = true;
+      count++;
     }
   }
 }
@@ -40,26 +46,92 @@ function placeMines() {
 function calculateNumbers() {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      if (!board[i][j].isMine) {
+      if (!grid[i][j].mine) {
         let count = 0;
         for (let x = -1; x <= 1; x++) {
           for (let y = -1; y <= 1; y++) {
-            const newRow = i + x;
-            const newCol = j + y;
-            if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
-              if (board[newRow][newCol].isMine) {
-                count++;
-              }
+            let ni = i + x;
+            let nj = j + y;
+            if (ni >= 0 && ni < rows && nj >= 0 && nj < cols && grid[ni][nj].mine) {
+              count++;
             }
           }
         }
-        board[i][j].number = count;
+        grid[i][j].number = count;
       }
     }
   }
 }
 
+function revealCell(i, j) {
+  if (i >= 0 && i < rows && j >= 0 && j < cols && !grid[i][j].revealed) {
+    grid[i][j].revealed = true;
+    if (grid[i][j].mine) {
+      gameOver = true;
+    } else if (grid[i][j].number === 0) {
+      for (let x = -1; x <= 1; x++) {
+        for (let y = -1; y <= 1; y++) {
+          revealCell(i + x, j + y);
+        }
+      }
+    }
+  }
+}
+
+function restartGame() {
+  grid = [];
+  gameOver = false;
+  bombsFound = 0;
+  createGrid();
+  placeMines();
+  calculateNumbers();
+}
+
+function mousePressed() {
+  if (!gameOver) {
+    let i = floor(mouseX / (width / rows));
+    let j = floor(mouseY / (height / cols));
+    revealCell(i, j);
+  }
+}
+
 function draw() {
   background(220);
-  // Draw the board here
+  let cellsRevealed = 0;
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      let cell = grid[i][j];
+      if (cell.revealed) {
+        cellsRevealed++;
+        if (cell.mine) {
+          fill(255, 0, 0);
+          ellipse(cell.x, cell.y, 0.8 * width / rows);
+        } else {
+          fill(200);
+          rect(cell.x - width / (2 * rows), cell.y - height / (2 * cols), width / rows, height / cols);
+          if (cell.number > 0) {
+            textAlign(CENTER, CENTER);
+            fill(0);
+            text(cell.number, cell.x, cell.y);
+          }
+        }
+      } else {
+        fill(150);
+        rect(cell.x - width / (2 * rows), cell.y - height / (2 * cols), width / rows, height / cols);
+      }
+    }
+  }
+  
+  if (cellsRevealed === rows * cols - totalMines) {
+    fill(0);
+    textAlign(CENTER, CENTER);
+    textSize(24);
+    text("All bombs found!", width / 2, height / 2);
+  }
+}
+
+function keyPressed() {
+  if (key === 'r' || key === 'R') {
+    restartGame();
+  }
 }
